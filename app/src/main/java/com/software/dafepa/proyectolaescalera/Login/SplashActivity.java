404139222061606ab771eecce958d31738d54daa
pablolira <pliras95@gmail.com>
2018-02-02 12:37:23 +0100

    Pantalla splash modificada y fusionada con login (Hace falta terminar de sacar la funcionalidad de login, ponerla en splash y borrar la activity de Login) Añadida Librería animaciones Añadido logo y fuentes

--- app/src/main/java/com/software/dafepa/proyectolaescalera/Login/SplashActivity.java
+++ app/src/main/java/com/software/dafepa/proyectolaescalera/Login/SplashActivity.java
@@ -1,36 +1,158 @@
 package com.software.dafepa.proyectolaescalera.Login;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.media.Image;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.daimajia.easing.Glider;
+import com.daimajia.easing.Skill;
 import com.software.dafepa.proyectolaescalera.PantallaPrincipal;
 import com.software.dafepa.proyectolaescalera.R;
+import com.software.dafepa.proyectolaescalera.Singletones.AplicacionManager;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private LinearLayout ly_logo_contenedor;
+    private int desplazamiento_y;
+    private boolean show_menu = true;
+    private int a = 0;
+    private LinearLayout ly_menu;
+    private TextView img_titulo;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ly_logo_contenedor = (LinearLayout) findViewById(R.id.ly_logo_contenedor);
+        ly_menu = (LinearLayout) findViewById(R.id.ly_menu);
+        ly_menu.setVisibility(View.GONE);
+        img_titulo = (TextView) findViewById(R.id.img_titulo);
+        Typeface font = Typeface.createFromAsset(this.getAssets(),"fonts/Pacifico-Regular.ttf");
+        img_titulo.setTypeface(font);
 
 
 
-       new Timer().schedule(new TimerTask() {
+        animacion();
+
+       /*new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
                 startActivity(new Intent(getApplicationContext(), LoginActivity.class));
                 finish();
             }
-        }, 4000);
+        }, 4000);*/
 
 
 
+    }
+
+
+    private void animacion(){
+        final long duracion_fadein = 800;
+        final int duracion_desplazamiento = 500;
+
+        desplazamiento_y =  -(AplicacionManager.getInstance().getScreen_height()*570)/2960;
+
+        YoYo.with(Techniques.ZoomInDown).duration(duracion_fadein).repeat(0).playOn(ly_logo_contenedor);
+
+
+        new CountDownTimer(duracion_fadein+100, duracion_fadein) {
+
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            public void onFinish() {
+
+                YoYo.with(Techniques.Pulse).duration(duracion_fadein).repeat(0).playOn(ly_logo_contenedor);
+                new CountDownTimer(duracion_fadein, duracion_fadein) {
+
+                    public void onTick(long millisUntilFinished) {
+
+                    }
+
+                    public void onFinish() {
+                        if (show_menu) {
+                            AnimatorSet set = new AnimatorSet();
+                            set.playTogether(
+                                    Glider.glide(Skill.CubicEaseOut, 2000,
+                                            ObjectAnimator.ofFloat(ly_logo_contenedor, "translationY", 0, desplazamiento_y))
+
+                            );
+
+                            set.setDuration(duracion_desplazamiento);
+                            set.start();
+
+
+
+
+                            new CountDownTimer(duracion_desplazamiento, 1 ){
+
+                                public void onTick(long millisUntilFinished) {
+                                    a+=5;
+                                    if(a >255){
+                                        a = 255;
+                                    }
+                                    //ly_logo_contenedor.setBackgroundColor(Color.argb(a, 145, 132, 214));
+                                }
+
+                                @Override
+                                public void onFinish() {
+
+                                }
+                            }.start();
+                            showMenu();
+                        }else{
+
+
+
+
+
+                        }
+                    }
+                }.start();
+
+            }
+        }.start();
+    }
+
+    public void showMenu(){
+
+        ly_menu.setVisibility(View.VISIBLE);
+        ly_menu.setAlpha(0);
+        final int duracion_fade = 500;
+        YoYo.with(Techniques.FadeIn).duration(duracion_fade).repeat(0).playOn(ly_menu);
+        new CountDownTimer(duracion_fade+100, duracion_fade + 100) {
+
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            public void onFinish() {
+                /*btn_registro.setEnabled(true);
+                btn_iniciosesion.setEnabled(true);
+                btn_ayuda.setEnabled(true);*/
+            }
+        }.start();
     }
 }
---
+++ app/src/main/java/com/software/dafepa/proyectolaescalera/Singletones/AplicacionManager.java
@@ -0,0 +1,78 @@
+package com.software.dafepa.proyectolaescalera.Singletones;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.graphics.Typeface;
+import android.view.Display;
+
+
+
+public class AplicacionManager {
+    private static AplicacionManager instance = null;
+
+    private int screen_width_ = 0;
+    private int screen_height_ = 0;
+
+    private Typeface main_font_= null;
+    //private Typeface neuropool_ = null;
+
+    private Activity a_init_screen_ = null;
+    private Boolean show_dialog_ = true;
+
+    private AplicacionManager() {
+        // Exists only to defeat instantiation.
+    }
+    public static AplicacionManager getInstance() {
+        if(instance == null) {
+            instance = new AplicacionManager();
+        }
+        return instance;
+    }
+
+
+    //Funcion para inicializar el singleton
+    public void init(Activity a){
+        a_init_screen_ = a;
+        //Reogida de tamano de pantalla
+        Display display = a.getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        screen_width_ = size.x;
+        screen_height_ = size.y;
+
+    }
+
+
+
+    public Activity getInitScreen(){
+        return a_init_screen_;
+    }
+
+    public int getScreen_width() {
+        return screen_width_;
+    }
+
+    public int getScreen_height() {
+        return screen_height_;
+    }
+
+    public Typeface getMainFont() {
+        return main_font_;
+    }
+
+    public void dialogShowed(){
+        show_dialog_ = false;
+    }
+
+    public boolean needToShow(){
+        return  show_dialog_;
+    }
+
+    /*public Typeface getNeuropool() {
+        return neuropool_;
+    }*/
+
+
+
+
+}
