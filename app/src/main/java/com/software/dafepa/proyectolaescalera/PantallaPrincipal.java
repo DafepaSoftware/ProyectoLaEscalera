84a0a644ba4ee82de4c9858ef07e60b3a0975da6
Fer <ferperezltbl@gmail.com>
2018-02-09 09:49:10 +0100

    Fernando hace cosas

--- app/src/main/java/com/software/dafepa/proyectolaescalera/PantallaPrincipal.java
+++ app/src/main/java/com/software/dafepa/proyectolaescalera/PantallaPrincipal.java
@@ -1,6 +1,7 @@
 package com.software.dafepa.proyectolaescalera;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,17 +9,30 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PantallaPrincipal extends AppCompatActivity {
 
     private Toolbar myToolbar;
     private DrawerLayout myDrawer;
+    private Button btn_editUser;
+    private Activity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_principal);
 
+        activity=this;
+        btn_editUser = (Button) findViewById(R.id.btn_editarUsuario);
+        btn_editUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity, EditUser.class);
+                startActivity(intent);
+            }
+        });
 
 
         toolbarCode();
