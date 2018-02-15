5dd79f2d344cc4be6ce6d5e55bcd97b0bfcbbe7c
pablolira <pliras95@gmail.com>
2018-02-15 11:46:09 +0100

    Modificaciones en pantalla principal añadido botón flotante y tab layout

--- app/src/main/java/com/software/dafepa/proyectolaescalera/PantallaPrincipal.java
+++ app/src/main/java/com/software/dafepa/proyectolaescalera/PantallaPrincipal.java
@@ -2,15 +2,27 @@ package com.software.dafepa.proyectolaescalera;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ExpandableListView;
+
+import com.software.dafepa.proyectolaescalera.Login.RegistroActivity;
 
 public class PantallaPrincipal extends AppCompatActivity {
 
@@ -18,6 +30,9 @@ public class PantallaPrincipal extends AppCompatActivity {
     private DrawerLayout myDrawer;
     private Button btn_editUser;
     private Activity activity;
+    private static int tabstart = 0;
+    private ViewPager mViewPager;
+    private SectionsPagerAdapter mSectionsPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +52,20 @@ public class PantallaPrincipal extends AppCompatActivity {
             }
         });
 
+        //Botón flotante
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(activity, NuevoEvento.class);
+                startActivity(intent);
+            }
+        });
 
 
         toolbarCode();
         navDrawerCode();
+        initViewPager();
     }
 
 
@@ -86,6 +111,106 @@ public class PantallaPrincipal extends AppCompatActivity {
         return true;
     }
 
+
+
+
+    /** CÓDIGO PARA EL SWIPE**/
+
+
+    private void initViewPager(){
+        tabstart = getIntent().getIntExtra("tab", 0);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+        tabLayout.setTabTextColors(getResources().getColor(R.color.color_fondo),
+                getResources().getColor(R.color.blanco));
+
+        mViewPager.setCurrentItem(tabstart);
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = null;
+            switch (getArguments().getInt(ARG_SECTION_NUMBER)){
+                case 1:{
+                    rootView = inflater.inflate(R.layout.fragment_busco, container, false);
+
+
+                    break;
+                }
+                case 2:{
+                    rootView = inflater.inflate(R.layout.fragment_busco, container, false);
+
+                    break;
+                }
+            }
+            return rootView;
+        }
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Contigo";
+                case 1:
+                    return "Pipo";
+            }
+            return null;
+        }
+    }
 
 
 }
