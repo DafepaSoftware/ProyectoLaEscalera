2ebc55531e2e9518dd1998df3f956bdd08324319
pablolira <pliras95@gmail.com>
2018-02-16 14:02:28 +0100

    Nuevo evento modificado

--- app/src/main/java/com/software/dafepa/proyectolaescalera/NuevoEvento.java
+++ app/src/main/java/com/software/dafepa/proyectolaescalera/NuevoEvento.java
@@ -2,12 +2,47 @@ package com.software.dafepa.proyectolaescalera;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class NuevoEvento extends AppCompatActivity {
+
+    private Toolbar myToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo_evento);
+        toolbarCode();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void toolbarCode(){
+        myToolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        myToolbar.setElevation(0.0f);
+        setSupportActionBar(myToolbar);
+        myToolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (item.getItemId()) {
+            case android.R.id.home: {
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
