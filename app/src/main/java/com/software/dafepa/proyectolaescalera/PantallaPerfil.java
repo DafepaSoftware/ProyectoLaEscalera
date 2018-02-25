e4a4ac08ee9bd72b08d3bf2192b003a3b415de01
Fer <ferperezltbl@gmail.com>
2018-02-25 13:46:31 +0100

    Fernando hace cosas

--- app/src/main/java/com/software/dafepa/proyectolaescalera/PantallaPerfil.java
+++ app/src/main/java/com/software/dafepa/proyectolaescalera/PantallaPerfil.java
@@ -1,19 +1,35 @@
 package com.software.dafepa.proyectolaescalera;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class PantallaPerfil extends AppCompatActivity {
 
     private Toolbar myToolbar;
+    private Button editar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_perfil);
+
+        editar = (Button) findViewById(R.id.btn_editar);
+
+        editar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pantallaeditar = new Intent(PantallaPerfil.this,EditUser.class );
+                startActivity(pantallaeditar);
+            }
+        });
+
         toolbarCode();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
