195082d1b81fb63e30682f99b0abef53e2a30c02
Fer <ferperezltbl@gmail.com>
2018-02-26 17:50:18 +0100

    Fernando hace cosas

--- app/src/main/java/com/software/dafepa/proyectolaescalera/InfoElemeneto.java
+++ app/src/main/java/com/software/dafepa/proyectolaescalera/InfoElemeneto.java
@@ -1,9 +1,13 @@
 package com.software.dafepa.proyectolaescalera;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,6 +15,8 @@ public class InfoElemeneto extends AppCompatActivity {
 
     private Toolbar myToolbar;
     private Button btnCancelar;
+    private Activity activity;
+
 
 
     @Override
@@ -18,15 +24,7 @@ public class InfoElemeneto extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_elemeneto);
 
-        btnCancelar = (Button) findViewById(R.id.btn_cancel);
 
-        btnCancelar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                finish();
-            }
-        });
 
 
 
@@ -43,4 +41,41 @@ public class InfoElemeneto extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (item.getItemId()) {
+            case android.R.id.home: {
+                finish();
+                return true;
+            }
+            case R.id.toolbar_acept: {
+                crearEvento();
+                return true;
+            }
+            case R.id.toolbar_cancel: {
+                cancelarEvento();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void crearEvento(){
+        finish();
+    }
+
+    private void cancelarEvento(){
+        new AlertDialog.Builder(activity).setMessage("¿Deseas salir?\n\nLos datos introducidos no " +
+                "se enviarán").setPositiveButton("Si", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        }).setNegativeButton("No", null).show();
+    }
 }
+
