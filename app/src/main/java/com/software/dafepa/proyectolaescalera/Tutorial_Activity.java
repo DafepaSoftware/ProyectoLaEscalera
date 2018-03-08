200c39edf5c45b8dd80ac84b3268982bc3ea7d97
Fer <ferperezltbl@gmail.com>
2018-03-08 17:47:29 +0100

    Strings

--- app/src/main/java/com/software/dafepa/proyectolaescalera/Tutorial_Activity.java
+++ app/src/main/java/com/software/dafepa/proyectolaescalera/Tutorial_Activity.java
@@ -3,11 +3,31 @@ package com.software.dafepa.proyectolaescalera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.robertlevonyan.views.expandable.Expandable;
+import com.robertlevonyan.views.expandable.ExpandingListener;
+
 public class Tutorial_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial_);
+
+        final Expandable expandable = findViewById(R.id.expandable);
+
+        expandable.setExpandingListener(new ExpandingListener() {
+            @Override
+            public void onExpanded() {
+                expandable.changeBackgroundColor(R.color.moradete); // Set a custom background color for layout
+
+            }
+
+            @Override
+            public void onCollapsed() {
+                //some stuff on collapse
+            }
+        });
+
+
     }
 }
